f2f8c3f737467d45860090b5f50bd6af88b9fb71
Andrei Dulceanu <dulceanu@adobe.com>
2017-10-20 16:00:05 +0300

    Added BasicWriteTest with a scenario from DataStoreTestBase

--- oak-run-commons/src/main/java/org/apache/jackrabbit/oak/fixture/SegmentTarFixture.java
+++ oak-run-commons/src/main/java/org/apache/jackrabbit/oak/fixture/SegmentTarFixture.java
@@ -230,7 +230,7 @@ class SegmentTarFixture extends OakFixture {
                 builder.withBlobStore(blobStore);
             } else {
                 blobStoreFixtures[n + i] = BlobStoreFixture.create(parentPath, true, dsCacheSize, statsProvider);
-                blobStore = blobStoreFixtures[n + i].setUp();
+                builder.withBlobStore(blobStoreFixtures[n + i].setUp());
             }
         }
         
@@ -315,11 +315,11 @@ class SegmentTarFixture extends OakFixture {
         
         if (blobStoreFixtures != null) {
             for (BlobStoreFixture bsf : blobStoreFixtures) {
-                bsf.tearDown();
+//                bsf.tearDown();
             }
         }
         
-        FileUtils.deleteQuietly(parentPath);
+//        FileUtils.deleteQuietly(parentPath);
     }
 
     public BlobStoreFixture[] getBlobStoreFixtures() {
