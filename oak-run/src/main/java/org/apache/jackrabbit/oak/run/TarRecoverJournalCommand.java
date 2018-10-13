56214faf58efe460e66d195ea3af370b89a7fbe8
Andrei Dulceanu <dulceanu@adobe.com>
2018-10-13 16:07:49 +0300

    Added option to select a specific tar file

--- oak-run/src/main/java/org/apache/jackrabbit/oak/run/TarRecoverJournalCommand.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/run/TarRecoverJournalCommand.java
@@ -5,6 +5,7 @@ import org.apache.jackrabbit.oak.segment.tool.JournalRecover;
 
 import java.io.File;
 
+import joptsimple.ArgumentAcceptingOptionSpec;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
@@ -16,18 +17,22 @@ public class TarRecoverJournalCommand implements Command {
         OptionParser parser = new OptionParser();
         OptionSpec<String> directoryArg = parser.nonOptions(
                 "Path to segment store (required)").ofType(String.class);
+        ArgumentAcceptingOptionSpec<String> tarFile = parser.accepts(
+                "tarFile", "name of the tar file to be checked for possible super roots")
+                .withRequiredArg().ofType(String.class);
         OptionSet options = parser.parse(args);
 
         String path = directoryArg.value(options);
 
         if (path == null) {
-            System.err.println("Recover an outdated journal from a file store. Usage: tar-recover-journal [path]");
+            System.err.println("Recover an outdated journal from a file store. Usage: tar-recover-journal [path] --tarFile NAME_OF_TAR_TO_CHECK");
             parser.printHelpOn(System.err);
             System.exit(-1);
         }
 
         int code = JournalRecover.builder()
             .withPath(new File(path))
+            .withTarFile(tarFile.value(options))
             .build()
             .run();
 
